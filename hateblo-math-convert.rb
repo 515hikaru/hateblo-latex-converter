b051d6fc6f06c12e08a98d9d571a679c2965de5b
Hikaru <12kojima.takahiro@gmail.com>
2016-07-08 11:20:51 +0900

    コメントを追加

--- hateblo-math-convert.rb
+++ hateblo-math-convert.rb
@@ -15,6 +15,7 @@ class HatebloMathText
     end
   end
 
+  # debug 用の関数, 処理後のテキストを表示
   def print_line
     check_and_replace
     @target_text.each do |line|
@@ -22,6 +23,7 @@ class HatebloMathText
     end
   end
 
+  # $...$ を [tex:{...}] に
   def replace_dollars(line)
     return unless line =~ /\$*\$/
     line.sub!(/\$/, '[tex:{')
@@ -29,17 +31,20 @@ class HatebloMathText
     replace_dollars(line)
   end
 
+  # \begin{equation} .. \end{equation} を [tex:{\begin{equation}...\end{equation}}] に
   def replace_environment(line)
     line.insert(0, '[tex:{') if line =~ /\\begin{.*}/
     line.insert(-2, '}]') if line =~ /\\end{.*}/
   end
 
+  # _ と ^ を \_, \^ にする
   def escape_symbol(line)
     return unless line =~ /[tex:{.*}]/
     line.gsub!(/\^/, '\^')
     line.gsub!(/\_/, '\_')
   end
 
+  # 置換を実行
   def check_and_replace
     @target_text.each do |line|
       replace_dollars(line)
@@ -48,6 +53,7 @@ class HatebloMathText
     end
   end
 
+  # ファイル書き込み
   def write_text
     check_and_replace
     File.open(@file_name, 'w') do |f|
@@ -58,5 +64,9 @@ class HatebloMathText
   end
 end
 
-target = HatebloMathText.new(ARGV[0])
-target.write_text
+# main
+
+if __FILE__ == $0
+  target = HatebloMathText.new(ARGV[0])
+  target.write_text
+end
